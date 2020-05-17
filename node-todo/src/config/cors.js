700827e73674ccbfd65e7f10ba9449a01bb59dc0
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-05-17 19:57:21 -0300

    fix(back): enable cors and fix headers typo

--- node-todo/src/config/cors.js
+++ node-todo/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-    res.header('Acces-Control-Allow-Origin', '*')
-    res.header('Acess-Control-Allow-Headers', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Acces-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Acesss-Control-Allow-Headers', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 }
