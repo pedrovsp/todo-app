c2dda220cbeba9c579e0ab3bf5c715bfaefc29d4
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-05-17 12:41:59 -0300

    feat(front): add about and todo component

---
+++ react-todo/src/about/About.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+export interface AboutProps { }
+
+export const About: FunctionComponent<AboutProps> = (props: AboutProps) => {
+    return (
+        <div>
+            About
+        </div>
+    )
+} 
---
+++ react-todo/src/todo/Todo.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export class Todo extends Component {
+    render() {
+        return (<div>Todo</div>);
+    }
+}
