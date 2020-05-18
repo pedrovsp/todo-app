945fabe41835fe79eeacb3e4d2c2a1c45c73e35a
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-05-17 21:51:32 -0300

    feat(front): add option to disable btn

--- react-todo/src/layout/button/Button.tsx
+++ react-todo/src/layout/button/Button.tsx
@@ -6,12 +6,15 @@ interface Props {
     styles: string;
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
     icon: string;
+    disabled?: boolean;
 }
 
 const Button = (props: Props) => {
     return (
         <If condition={!props.hide}>
-            <button className={`btn btn-${props.styles}`} onClick={props.onClick}>
+            <button className={`btn btn-${props.styles}`}
+                disabled={(props.disabled || false)}
+                onClick={props.onClick}>
                 <i className={`fa fa-${props.icon}`}></i>
             </button>
         </If>
