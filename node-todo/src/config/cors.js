f4a095c523fa8743a92615b9147b5c23431d22df
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-05-17 22:02:43 -0300

    bugfix(back):  fix typo on cors

--- node-todo/src/config/cors.js
+++ node-todo/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Acesss-Control-Allow-Headers', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 }
