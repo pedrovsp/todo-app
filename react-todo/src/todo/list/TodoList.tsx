8642e36026d5f7eb40433872ba915082dcf32a2b
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-05-17 21:51:00 -0300

    feat(front): add todo list

--- react-todo/src/todo/list/TodoList.tsx
+++ react-todo/src/todo/list/TodoList.tsx
@@ -1,15 +1,53 @@
 import React from 'react'
+import { TodoItem } from '../model/TodoItem.model'
+import Button from '../../layout/button/Button';
+import './TodoList.scss';
 
 interface Props {
-
+    list: [],
+    handleEdit: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    handleDelete: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    handleCheck: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const TodoList = (props: Props) => {
-    return (
-        <div>
 
-        </div>
+    let listItems = (props.list || []).map((item: TodoItem, i) => {
+        return (
+            <tr key={i}>
+                <td>{i}</td>
+                <td>{item.description}</td>
+                <td>{item.createdAt}</td>
+                <td>
+                    <i className={'status fa fa-check-square ' + (item.done ? 'checked' : '')}></i>
+                </td>
+                <td className='d-flex justify-content-between'>
+                    <Button styles='primary' icon='check-circle'
+                        disabled={item.done} onClick={props.handleCheck}></Button>
+                    <Button styles='secondary' icon='edit'
+                        disabled={item.done} onClick={props.handleEdit}></Button>
+                    <Button styles='danger' icon='trash' onClick={props.handleDelete}></Button>
+                </td>
+            </tr >
+        )
+    });
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Description</th>
+                    <th>Created Date</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listItems}
+            </tbody>
+        </table>
     )
+
 }
 
 export default TodoList
