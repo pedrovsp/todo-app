f8998e80bde5018b919d6ed6898b18118a9e4258
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-05-26 23:54:19 -0300

    feat(redux): add multi redux

--- react-todo/src/index.js
+++ react-todo/src/index.js
@@ -6,10 +6,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './main/reducers';
 import promise from 'redux-promise';
+import multi from 'redux-multi';
 
 import { App } from './main/App';
 
-const store = applyMiddleware(promise)(createStore)(reducers);
+const store = applyMiddleware(multi, promise)(createStore)(reducers);
 ReactDOM.render(
     <Provider store={store}>
         <App></App>
